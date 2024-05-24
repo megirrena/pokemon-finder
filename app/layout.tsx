cdea28d5efdba8006554977736c6d9004a4f47bd
megirrena <megirrena12@gmail.com>
2024-05-24 16:11:35 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <main className="flex min-h-screen flex-col items-center  p-24">
+            <div className="z-10 w-full max-w-5xl items-center justify-between  text-sm lg:flex">
+              <h2 className="text-2xl text-bold"> Pokemon Finder</h2>
+            </div>
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
